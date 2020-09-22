70261ed97d32d2856aa8f6fd943e1afa1aafd6d8
MaSungHo <tonem123@naver.com>
2020-09-22 21:43:28 +0900

    create index, create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  
+  get 'home/index'
+  #get 'home/index' => 'home#index'
+  get 'home/new'
+  #get 'home/new' => 'home#new'
+  #get 'home/create'
+  #get 'home/create' => 'home#create'
+  post 'home/create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
