e5d11ef48b3a6d73082f4a74b96ea73924199e31
MaSungHo <tonem123@naver.com>
2020-09-23 11:38:51 +0900

    Use link_to instead of a tag

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   #get 'home/create' => 'home#create'
   post 'home/create'
   
-  get 'home/destroy/:post_id' => 'home#destroy'
+  get 'home/destroy/:post_id' => 'home#destroy', as: 'post_destroy'
+  #get 'home/destroy/:post_id', to: 'home#destroy', as: 'post_destroy'와 같다
   # 이름이 같지 않으니 더이상 축약해서 나타낼 수 없음.
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
