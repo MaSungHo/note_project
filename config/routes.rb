d1f3fad6ba452f2a75483b8a522f6d5511634544
MaSungHo <tonem123@naver.com>
2020-09-23 14:14:38 +0900

    Use scaffold form

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   root 'home#index'
   
-  get 'home/index'
-  #get 'home/index' => 'home#index'
-  get 'home/new'
-  #get 'home/new' => 'home#new'
-  #get 'home/create'
-  #get 'home/create' => 'home#create'
-  post 'home/create', as: 'posts'
+  resources :posts, controller: :home
+  # get 'home/index'
+  # #get 'home/index' => 'home#index'
+  # get 'home/new'
+  # #get 'home/new' => 'home#new'
+  # #get 'home/create'
+  # #get 'home/create' => 'home#create'
+  # post 'home/create', as: 'posts'
   
-  get 'home/destroy/:post_id' => 'home#destroy', as: 'post_destroy'
-  #get 'home/destroy/:post_id', to: 'home#destroy', as: 'post_destroy'와 같다
-  # 이름이 같지 않으니 더이상 축약해서 나타낼 수 없음.
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'home/destroy/:post_id' => 'home#destroy', as: 'post_destroy'
+  # #get 'home/destroy/:post_id', to: 'home#destroy', as: 'post_destroy'와 같다
+  # # 이름이 같지 않으니 더이상 축약해서 나타낼 수 없음.
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'home/edit/:post_id' => 'home#edit'
+  # get 'home/edit/:post_id' => 'home#edit'
 
-  patch 'home/update/:post_id' => 'home#update', as: 'post'
+  # patch 'home/update/:post_id' => 'home#update', as: 'post'
 end
