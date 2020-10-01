3a54a7244760602e9f0cc2ae6eda4ac9edaeaddd
MaSungHo <tonem123@naver.com>
2020-10-01 13:39:01 +0900

    Add user_comment references

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_035148) do
+ActiveRecord::Schema.define(version: 2020_10_01_043120) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 2020_10_01_035148) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
